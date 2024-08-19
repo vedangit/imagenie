c31e7abe1554891a70159e61aba8a36a8a05001e
vedangit <vedangithokal@gmail.com>
2024-08-19 12:32:14 +0530

    init

---
+++ components/shared/Checkout.tsx
@@ -0,0 +1,16 @@
+"use client";
+/*
+import { loadStripe } from "@stripe/stripe-js";
+import { useEffect } from "react";
+
+import { useToast } from "@/components/ui/use-toast";
+import { checkoutCredits } from "@/lib/actions/transaction.action"; */
+
+import { Button } from "../ui/button";
+
+const Checkout = () => {(
+  <h1>Checkout page</h1>
+  );
+};
+
+export default Checkout;
